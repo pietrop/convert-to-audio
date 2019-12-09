4bd62f367f1a74d9ea410d647cf24ca442fc09e8
Eimi Okuno <eimi.okuno@bbc.co.uk>
2019-12-09 13:54:59 +0000

    Adding stream version of the function

--- src/example-usage.js
+++ src/example-usage.js
@@ -1,11 +1,20 @@
-const convertToAudio = require("./index.js");
-
+const { convertToAudio, convertStreamToAudio } = require("./index.js");
+const fs = require("fs");
 const url = "https://download.ted.com/talks/KateDarling_2018S-950k.mp4";
 const audioFileOutput = "./ted-talk.wav";
 
 convertToAudio(url, audioFileOutput)
   .then(newFile => {
     console.log(newFile);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+const writeStream = fs.createWriteStream(audioFileOutput);
+convertStreamToAudio(url, writeStream)
+  .then(() => {
+    console.log(audioFileOutput);
   })
   .catch(err => {
     console.error(err);
